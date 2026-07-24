6a25892b992072f0b74e4f868f51887ace01de74
Rao Waleed <rao.waleed.nadeem@gmail.com>
2026-07-24 21:28:12 +0500

    Updated chat service to communicate with repository not directly with database

--- backend/app/services/chat_service.py
+++ backend/app/services/chat_service.py
@@ -1,9 +1,18 @@
+from sqlalchemy.orm import Session
+
 from app.schemas.chat import (
     ChatRequest,
     ChatResponse,
 )
-from app.providers.groq_provider import (
-    GroqProvider,
+
+from app.providers.groq_provider import GroqProvider
+
+from app.repositories.chat_repository import (
+    create_chat,
+)
+
+from app.repositories.message_repository import (
+    create_message,
 )
 
 provider = GroqProvider()
@@ -13,9 +22,33 @@ class ChatService:
 
     def generate_response(
         self,
+        db: Session,
         request: ChatRequest,
     ) -> ChatResponse:
 
-        reply = provider.generate_response(request.message)
+        chat = create_chat(
+            db=db,
+            title="New Chat",
+        )
 
-        return ChatResponse(message=reply)
+        create_message(
+            db=db,
+            chat_id=chat.id,
+            role="user",
+            content=request.message,
+        )
+
+        reply = provider.generate_response(
+            request.message,
+        )
+
+        create_message(
+            db=db,
+            chat_id=chat.id,
+            role="assistant",
+            content=reply,
+        )
+
+        return ChatResponse(
+            message=reply,
+        )
